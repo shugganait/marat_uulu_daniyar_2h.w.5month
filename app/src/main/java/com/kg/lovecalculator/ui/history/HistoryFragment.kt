713bb5048b67841c7debfc4bfb6e4a6adb926629
shugganait <daniarm209@gmail.com>
2023-04-25 03:03:20 +0600

    добавил некоторые верстки

--- app/src/main/java/com/kg/lovecalculator/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/kg/lovecalculator/ui/history/HistoryFragment.kt
@@ -5,14 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.kg.lovecalculator.R
+import com.kg.lovecalculator.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initNavigations()
+    }
+
+    private fun initNavigations() {
+        binding.apply {
+            btnNavigateHome.setOnClickListener {
+                findNavController().navigateUp()
+            }
+            btnNavigateHistory.setOnClickListener {
+                findNavController().navigate(R.id.historyFragment)
+            }
+        }
     }
 }
